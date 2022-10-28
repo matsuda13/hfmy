6768abe5f84e4572344a956db40350586aea7049
Ryunosuke Funasako <e205732@ie.u-ryukyu.ac.jp>
2022-10-28 19:41:04 +0900

    ドライバーが募集できるようにした

---
+++ hfmyApp/yui-loop/src/WaitingPassenger.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { WaitingPassengerType } from './types/WaitingPassengerType'
+import { FC } from 'react'
+
+interface WaitingPassengerProps {
+  waitingPassenger: WaitingPassengerType,
+}
+
+const WaitingPassenger: FC<WaitingPassengerProps> = (props) => {
+  return (
+    <div>{props.waitingPassenger.message}</div>
+  )
+}
+
+export default WaitingPassenger
---
+++ hfmyApp/yui-loop/src/types/WaitingPassengerType.ts
@@ -0,0 +1,3 @@
+export interface WaitingPassengerType {
+    message: string, 
+}
